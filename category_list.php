9d94e16ff5a2673e45bf3099d4f100b3f7902112
Pranav Bawa <pb389@afsaccess1.njit.edu>
2017-02-26 15:23:21 -0500

    added

--- category_list.php
+++ category_list.php
@@ -27,31 +27,35 @@ $statement->closeCursor();
         <tr>
             <th>Name</th>
             <th>&nbsp;</th>
-
+        </tr>        
+        <?php foreach ($categories as $category) : ?>
+        <tr>
+            <td><?php echo $category['categoryName']; ?></td>
+            <td>
+                <form action="delete_category.php" method="post">
+                    <input type="hidden" name="category_id"
+                           value="<?php echo $category['categoryID']; ?>"/>
+                    <input type="submit" value="Delete"/>
+                </form>
+            </td>
         </tr>
-	
-     <?php foreach ($categories as $category) : ?>
-     <tr>
- <td><?php echo $category['categoryName']; ?></td>
- <td>
-    <form action="delete_category.php" method="post">
-    <input type="Hidden" name="category_id" value="<?php echo $category=['categoryID']; ?>"/>
-    <input type="submit" value="Delete"/>
-    </form>
-    </td>
-    </tr>
+        <?php endforeach; ?>    
     </table>
 
     <h2>Add Category</h2>
+    <form action="add_category.php" method="post"
+          id="add_category_form">
+
+        <label>Name:</label>
+        <input type="text" name="name" />
+        <input id="add_category_button" type="submit" value="Add"/>
+    </form>
     
-   
-    <br> 	
     <p><a href="index.php">List Products</a></p>
 
-    </main>
-
-    <footer>
-        <p>&copy; <?php echo date("Y"); ?> My Guitar Shop, Inc.</p>
-    </footer>
+</main>
+<footer>
+    <p>&copy; <?php echo date("Y"); ?> My Guitar Shop, Inc.</p>
+</footer>
 </body>
 </html>
